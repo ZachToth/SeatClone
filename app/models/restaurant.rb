ca407c4e62004cfa1fc94bff969be3724af0a08f
Zachary Toth <zachtoth@gmail.com>
2015-02-12 16:45:34 -0500

    Can now view the main page without logging in

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,20 @@
 class Restaurant < ActiveRecord::Base
   has_many :reservations
   has_many :users, through: :reservations
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit 
+  end
+
+  def destroy
+  end
+
 end
