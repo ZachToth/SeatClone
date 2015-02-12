320090f07afdbdaf8149795b259bcc230641eea7
jmy19 <jasonmyers@rogers.com>
2015-02-12 15:29:24 -0500

    added cuisine to restaurants model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150211221551) do
+ActiveRecord::Schema.define(version: 20150212202756) do
 
   create_table "reservations", force: :cascade do |t|
     t.string   "user_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20150211221551) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "cuisine"
   end
 
   create_table "users", force: :cascade do |t|
