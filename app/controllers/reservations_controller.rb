974440eec7b5bb91b7dac4a776268fda4117f1d8
lauranjansen <lauran.jansen@gmail.com>
2015-02-12 19:24:14 -0500

    Fix reservation functionality

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,18 +1,23 @@
 class ReservationsController < ApplicationController
-before_filter :ensure_logged_in
+before_filter :ensure_logged_in, :load_restaurant
 
   def create
-      @reservation = @restaurant.reservation.build(reservation_params)
-      @reservation.user = current_user
+    @reservation = @restaurant.reservations.build(reservation_params)
+    @reservation.user = current_user
 
-      if @reservation.save
-        redirect_to restaurants_path
-      else
-        redirect_to restaurants_path
-      end
+    if @reservation.save
+      redirect_to restaurants_path, notice: "Reservation successfully made!"
+    else
+      render 'restaurants/show'
+    end
   end
 
+  private
   def reservation_params
-    reservation_params.require(:reservation).permit(:time, :date, :party_size)
+    params.require(:reservation).permit(:time, :date, :party_size, :restaurant_id)
+  end
+
+  def load_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
