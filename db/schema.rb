97d6e364566a522a4ad180eca6eee282d0772d31
Rob Modzelewski <bobmodzelewski@gmail.com>
2015-02-16 10:59:08 -0500

    Added capacity

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150212220046) do
+ActiveRecord::Schema.define(version: 20150213210749) do
 
   create_table "reservations", force: :cascade do |t|
     t.string   "user_id"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150212220046) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "cuisine"
+    t.integer  "capacity"
   end
 
   create_table "users", force: :cascade do |t|
